2b798346134368d35fde403283d601d384850752
vovvoy <ormonbaev.marsel@gmail.com>
2019-06-13 11:41:09 +0600

    task3 part2

---
+++ 3.py
@@ -0,0 +1,4 @@
+number = int (input())
+print ("The previous number for the number " ,  number  , " is ", + number - 1 , ".")
+print ("The next number for the number ", number," is ", number + 1, ".")
+
